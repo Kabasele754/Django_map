430e072e93e52c7f686f3d7704ee122b6cecee4a
Achille kabasele <pepexykabasele@gmail.com>
2022-10-30 15:20:00 +0100

    add a model and get data address

--- app_map/views.py
+++ app_map/views.py
@@ -1,35 +1,65 @@
-from multiprocessing import context
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
 import folium
 import geocoder
 
-# Create your views here.
-
-
+from .models import Search
 
 # Create your views here.
 
-class Home(TemplateView):
+def Home(request, template_name= "map/index.html"):
+    context = {}
+        
+    # Search address
+    address = Search.objects.all().last()
+    # location
+    location = geocoder.osm(address)
+    lat = location.lat
+    lng = location.lng
+    country = location.country
     
-    template_name= "map/index.html"
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # location
-        location = geocoder.osm('UK')
-        lat = location.lat
-        lng = location.lng
-        country = location.country
-        # map object
-        m = folium.Map(location=[19,-12,], zoom_start=2)
-        folium.Marker([4.594, -0.219],tooltip="click for more", popup='DRC').add_to(m)
+    # map object
+    m = folium.Map(location=[19,-12,], zoom_start=2)
+    #folium.Marker([4.594, -0.219],tooltip="click for more", popup='DRC').add_to(m)
+    
+    
+    folium.Marker([lat, lng],tooltip="click for more", popup=country).add_to(m)
+    #  get html representation of map object
+    m = m._repr_html_()
+        
+    context['m'] = m
+    return render(request, template_name, context)
+
+    
+
+# Create your views here.
+
+# class Home(View):
+    
+#     template_name= "map/index.html"
+    
+#     def get_context_data(self, request, **kwargs):
+#         context = {}
+        
+#         # Search address
+#         address = request.POST.get('seach')
+#         # location
+#         location = geocoder.osm(str(address))
+#         lat = location.lat
+#         lng = location.lng
+#         country = location.country
         
        
-        folium.Marker([lat, lng],tooltip="click for more", popup=country).add_to(m)
-        #  get html representation of map object
-        m = m._repr_html_()
+#         # map object
+#         m = folium.Map(location=[19,-12,], zoom_start=2)
+#         folium.Marker([4.594, -0.219],tooltip="click for more", popup='DRC').add_to(m)
+        
+       
+#         folium.Marker([lat, lng],tooltip="click for more", popup=country).add_to(m)
+#         #  get html representation of map object
+#         m = m._repr_html_()
             
-        context['m'] = m
-        return context
+#         context['m'] = m
+#         return context
     
